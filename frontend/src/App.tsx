dd8c5281d58291b3fc6b6e1c2d7d58998a3447a0
saradab-mindfire <saradab@mindfiresolutions.com>
2025-07-24 12:51:59 +0530

    fix: ignoring env and package-lock

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { trpc } from "./trpc";
 
@@ -9,13 +7,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        {user?.userDetails.nameFirst} {user?.userDetails.nameLast}
+        <h3>Welcome Abord</h3>
+        <h2>
+          {user?.userDetails.nameFirst} {user?.userDetails.nameLast}
+        </h2>
       </div>
     </>
   );
